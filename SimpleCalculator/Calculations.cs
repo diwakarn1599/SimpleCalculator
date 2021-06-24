235b13ecf8d511c807160297f04ead11314cec8f
Diwakar <diwakarn1599@gmail.com>
2021-06-24 13:26:03 +0530

    First Commit in SimpleCalculator

---
+++ SimpleCalculator/Calculations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    class Calculations
+    {
+        public int  Calculate(int operation, int num1,int num2)
+        {
+            int result = 0;
+            switch (operation)
+            {
+                case 1:
+                    result =  num1 + num2;
+                    break;
+                case 2:
+                    result =  Math.Abs(num1 - num2);
+                    break;
+                case 3:
+                    result =  num1 * num2;
+                    break;
+                case 4:
+                    result =  num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Enter Correct operation");
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2,operation,result;
+            Console.WriteLine("Which Operation do you want to perform?\n");
+            Console.WriteLine("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division");
+            operation = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first number\n");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number\n");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(operation + " " + num1 + " " + num2);
+            Calculations obj = new Calculations();
+            result = obj.Calculate(operation,num1,num2);
+            Console.WriteLine("The result is " + result);
+
+
+        }
+    }
+}
